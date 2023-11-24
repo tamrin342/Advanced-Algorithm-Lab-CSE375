696374aed8dfd781db282081ff4a909bb73b78d6
Tamrin Nahar Lam <137845267+tamrin342@users.noreply.github.com>
2023-11-25 00:02:32 +0600

    Add files via upload

---
+++ Subset_Sum.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool subset_sum(int n,int arr[],int sum)
+{
+int include,exclude;
+bool table[n+1][sum+1];
+for(int i=0; i<n+1; i++)
+{
+for(int j=0; j<sum+1; j++)
+{
+if(i==0)
+{
+if(j==0) table[i][j]=1;
+else table[0][j]=0;
+}
+else
+{
+include=table[i-1][j];
+if(j-arr[i-1]<0) exclude=0;
+else exclude=table[i-1][j-arr[i-1]];
+table[i][j]=include | exclude;
+}
+}
+}
+cout<<"Table ---> "<<endl;
+for(int i=0; i<n+1; i++)
+{
+for(int j=0; j<sum+1; j++)
+{
+if(table[i][j]==1) cout<<"T"<<" ";
+else cout<<"F"<<" ";
+}
+cout<<endl;
+}
+return table[n][sum];
+}
+int main()
+{
+int arr[] = {1,3,4,7,10};
+int sum;
+cout<<"Enter the value of sum : ";
+cin>>sum;
+int n = sizeof(arr)/sizeof(arr[0]);
+bool ans = subset_sum(n,arr,sum);
+cout<<endl;
+if(ans==1) cout<<"Yes, it's possible."<<endl;
+else cout<<"No, it's not possible."<<endl;
+}
